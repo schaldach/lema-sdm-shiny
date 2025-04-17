ba568f98ee4c0cc5c2a2bc271246b31c94de71d7
Schaldach <gabimorgado@gmail.com>
2025-04-17 12:19:18 -0300

    Limpando código

--- shiny.R
+++ shiny.R
@@ -17,18 +17,18 @@ ui <- page_sidebar(
 # Definindo servidor
 server <- function(input, output, session) {
   # Arquivo teste para desenvolvimento (substituindo os resultados dos modelos)
-  # raster_test <- terra::rast("data/ambiental/thetao_mean_baseline_2000_2019_depthsurf.nc")[[2]]
-  # 
-  # output$sdm_map <- renderLeaflet({
-  #   # ficar atento para aquele possível bug da paleta de cores, está certo dessa forma? apenas verificar
-  #   # o certo é usar dessa forma, colors=pal, mas parecia naquela ocasião que usar colors="viridis" funcionava melhor
-  #   pal <- colorNumeric(palette = "viridis", domain = values(raster_test), na.color = NA)
-  #   
-  #   leaflet() %>%
-  #     addProviderTiles("OpenStreetMap") %>%
-  #     addRasterImage(raster_test, colors = pal, opacity = 0.75) %>%
-  #     addLegend(pal = pal, values = values(raster_test), title = "Relative ocurrence rate")
-  # })
+  raster_test <- terra::rast("data/ambiental/thetao_mean_baseline_2010_2019_depthsurf.tif")
+
+  output$sdm_map <- renderLeaflet({
+    # ficar atento para aquele possível bug da paleta de cores, está certo dessa forma? apenas verificar
+    # o certo é usar dessa forma, colors=pal, mas parecia naquela ocasião que usar colors="viridis" funcionava melhor
+    pal <- colorNumeric(palette = "viridis", domain = values(raster_test), na.color = NA)
+
+    leaflet() %>%
+      addProviderTiles("OpenStreetMap") %>%
+      addRasterImage(raster_test, colors = pal, opacity = 0.75) %>%
+      addLegend(pal = pal, values = values(raster_test), title = "Relative ocurrence rate")
+  })
 }
 
 # Criando o aplicativo
